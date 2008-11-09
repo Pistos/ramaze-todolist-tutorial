7f95468639661e684570e37d136ac80b83f1e6ac
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-11-08 22:51:25 -0500

    Added create action.

--- src/main.rb
+++ src/main.rb
@@ -1,5 +1,12 @@
 class MainController < Ramaze::Controller
+  
   def index
     @tasks = Task.all
   end
+  
+  def create
+    Task.create( :description => request[ 'description' ] )
+    redirect Rs()
+  end
+  
 end
