dd1dfa95598845e68014f20db267040fd3c59aa7
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-11-09 18:00:17 -0500

    Added a little error checking and handling. Using FlashHelper.

--- src/main.rb
+++ src/main.rb
@@ -12,18 +12,46 @@ class MainController < Ramaze::Controller
   end
   
   def create
+    description = request[ 'description' ]
+    return if description.nil?
+    
+    title.strip!
+    if title.empty?
+      fail 'Please enter a description for the new task.'
+      redirect '/new'
+    end
+    
     Task.create( :description => h( request[ 'description' ] ) )
     redirect Rs( :/ )
   end
   
   def check_off( id )
-    Task[ id ].check_off
+    task = ensure_task_exists( id )
+    task.check_off
     redirect Rs( :/ )
   end
   
   def delete( id )
-    Task[ id ].delete
+    task = ensure_task_exists( id )
+    task.delete
     redirect Rs( :/ )
   end
+  
+  # -------------------------------------------------
+  
+  def fail( message )
+    flash[ :error ] = message
+  end
+  private :fail
+  
+  def ensure_task_exists( id )
+    task = Task[ id ]
+    if task.nil?
+      fail "No task with id #{id}."
+      redirect_referrer
+    end
+    task
+  end
+  private :ensure_task_exists
   
 end
