d3ffe9dcab1b860668c417debf95dee94aa599d5
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-11-08 22:08:39 -0500

    Added some sample data.

---
+++ sql/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tasks ( description ) VALUES ( 'Learn Ramaze basics.' );
+INSERT INTO tasks ( description ) VALUES ( 'Say hello in Ramaze IRC channel.' );
+INSERT INTO tasks ( description ) VALUES ( 'Write a blog post about Ramaze.' );
