6ef1352db9d3033899e63d68e05fadc1eeee3351
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-11-10 22:53:53 -0500

    Columnized schema.sql.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tasks (
-    id SERIAL PRIMARY KEY,
+    id          SERIAL          PRIMARY KEY,
     description VARCHAR( 1024 ) NOT NULL,
-    done BOOLEAN NOT NULL DEFAULT FALSE
+    done        BOOLEAN         NOT NULL DEFAULT FALSE
 );
